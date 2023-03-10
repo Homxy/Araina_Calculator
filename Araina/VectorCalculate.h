092e8ae862676fc320fcac66a2e54d43668a8159
TannabiZz <120448549+CannabiZz9@users.noreply.github.com>
2023-03-10 14:32:13 +0700

    Update VectorCalculate.h

--- Araina/VectorCalculate.h
+++ Araina/VectorCalculate.h
@@ -3,18 +3,10 @@
 #include <cmath>
 using namespace std;
 
-float dot_vector(vector<float> v1, vector<float> v2) {
-
-    /* dot v->u =   u1v1+u2v2+u3v3 */
-
-    float result = 0;
-    int dim = v1.size();
-
-    for (int i = 0; i < dim; i++) {
-    result += v1[i] * v2[i];
-    }
-
-    return result;
+float dotProduct(vector<float> x, vector<float> y){
+	int sum = 0;
+	for(unsigned int i = 0; i < x.size() ; i++) sum += (x.at(i)*y.at(i));	
+	return sum;
 }
 
 vector<float> cross_vector(vector<float> vec1, vector<float> vec2) {
